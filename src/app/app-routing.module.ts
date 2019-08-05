60cca0f5e0968bd4655cd7d9c99b86aec3a33eee
Ricky Goacher <rickyleegoacher@hotmail.co.uk>
2019-08-05 20:54:54 +0100

    Added application routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', component: HomeComponent },
+	{ path: '/about', component: AboutComponent },
+	{ path: '/contact', component: ContactComponent },
+	{ path: '/portfolio', component: PortfolioComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [
+	HomeComponent,
+	AboutComponent,
+	ContactComponent,
+	PortfolioComponent
+];
