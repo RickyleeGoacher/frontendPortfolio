18ee80a7df475fbd0c0b113e25ccb9b4ce6449ee
Ricky Goacher <rickyleegoacher@hotmail.co.uk>
2019-08-06 18:09:33 +0100

    Angular forms + reactive forms imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
