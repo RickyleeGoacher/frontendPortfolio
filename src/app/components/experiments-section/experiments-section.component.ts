57c4bb081ebfb0dbc553c6371136ac5d26da2869
Ricky Goacher <rickyleegoacher@hotmail.co.uk>
2019-08-06 17:44:36 +0100

    Experiments section HTML added + link to project section in typescript file

--- src/app/components/experiments-section/experiments-section.component.ts
+++ src/app/components/experiments-section/experiments-section.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-experiments-section',
   templateUrl: './experiments-section.component.html',
-  styleUrls: ['./experiments-section.component.sass']
+  styleUrls: ['../project-section/project-section.component.sass']
 })
 export class ExperimentsSectionComponent implements OnInit {
 
