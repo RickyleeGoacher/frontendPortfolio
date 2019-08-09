f130613414c776b5563895cf9ed0b5b9e682d562
Ricky Goacher <rickyleegoacher@hotmail.co.uk>
2019-08-09 17:04:16 +0100

    Project routes and body parser added

--- api/index.js
+++ api/index.js
@@ -3,6 +3,11 @@ const app = express();
 const mongoose = require('mongoose');
 const router = express.Router();
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const ProjectRoutes = require('/routes/projects');
+
+
+app.use('/api/projects', ProjectRoutes);
 
 const PORT = process.env.PORT || 3000;
 
