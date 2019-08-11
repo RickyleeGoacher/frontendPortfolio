0e69acf5a89928a79a39ea2a3ec0ab9a88c9702e
Ricky Goacher <rickyleegoacher@hotmail.co.uk>
2019-08-11 18:09:40 +0100

    Experiments schema model added

---
+++ api/models/experiments-model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+
+mongoose.connection.once('open', function() {
+	console.log('Connection has been made!');	
+}).on('error', function(error){
+	console.log('Error is: ', error);
+});
+
+const Schema = mongoose.Schema;
+
+const ExperimentsSchema = new Schema({
+	title: {
+		type: String,
+		required: true
+	},
+	description: {
+		type: String,
+		required: true
+	},
+	url: {
+		type: String,
+		required: true
+	},
+	icon: {
+		type: String,
+		required: true
+	}
+});
+
+module.exports = mongoose.model('Experiment', ExperimentSchema);
