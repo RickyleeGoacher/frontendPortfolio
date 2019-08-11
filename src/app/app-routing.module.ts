bda619d15848e57221d2452c62c0c4357dc7162c
Ricky Goacher <rickyleegoacher@hotmail.co.uk>
2019-08-11 20:37:33 +0100

    Login, register, dashboard, create about, create experiments and create projects added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,24 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { LoginComponent } from './admin/login/login.component';
+import { RegisterComponent } from './admin/register/register.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { CreateAboutComponent } from './admin/create-about/create-about.component';
+import { CreateExperimentComponent } from './admin/create-experiment/create-experiment.component';
+import { CreateProjectComponent } from './admin/create-project/create-project.component';
 
 const routes: Routes = [
 	{ path: '', component: HomeComponent },
 	{ path: 'about', component: AboutComponent },
 	{ path: 'contact', component: ContactComponent },
-	{ path: 'portfolio', component: PortfolioComponent }
+	{ path: 'portfolio', component: PortfolioComponent },
+	{ path: 'login', component: LoginComponent },
+	{ path: 'register', component: RegisterComponent },
+	{ path: 'dashboard', component: DashboardComponent },
+	{ path: 'create-about', component: CreateAboutComponent },
+	{ path: 'create-experiment', component: CreateExperimentComponent },
+	{ path: 'create-project', component: CreateProjectComponent }
 ];
 
 @NgModule({
@@ -22,5 +34,11 @@ export const routingComponents = [
 	HomeComponent,
 	AboutComponent,
 	ContactComponent,
-	PortfolioComponent
+	PortfolioComponent,
+	LoginComponent,
+	RegisterComponent,
+	DashboardComponent,
+	CreateAboutComponent,
+	CreateExperimentComponent,
+	CreateProjectComponent
 ];
