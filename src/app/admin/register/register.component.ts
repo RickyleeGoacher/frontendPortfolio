4f0921a383490cf66a0eaa15f2d21575080cc462
Ricky Goacher <rickyleegoacher@hotmail.co.uk>
2019-08-11 22:19:46 +0100

    Register form added

--- src/app/admin/register/register.component.ts
+++ src/app/admin/register/register.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.sass']
+  styleUrls: ['../login/login.component.sass']
 })
 export class RegisterComponent implements OnInit {
 
