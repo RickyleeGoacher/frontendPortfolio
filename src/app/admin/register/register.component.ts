a2d450d72dcf8c2468625de50bbf4481d2c1dc23
Ricky Goacher <rickyleegoacher@hotmail.co.uk>
2019-08-12 13:56:15 +0100

    Register subscribed to userservice

--- src/app/admin/register/register.component.ts
+++ src/app/admin/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+	registerForm:FormGroup = new FormGroup({
+    	email:new FormControl(null,[Validators.email,Validators.required]),
+    	password:new FormControl(null,Validators.required),
+    	password2:new FormControl(null,Validators.required),
+      	adminSecret:new FormControl(null,Validators.required)
+  	})
 
-  ngOnInit() {
-  }
+  constructor(private router: Router, private userService: UserService) { }
 
+  	public errorMessage: string = '';
+
+  	ngOnInit() {
+  	}
+
+  	moveToLogin() {
+  		this.router.navigate(['/']);
+  	}
+
+  	onSubmit(){
+    	if(!this.registerForm.valid){
+    	console.log('Invalid Form'); 
+      	return;
+    	}
+    	this.userService.register(JSON.stringify(this.registerForm.value))
+        .subscribe(
+      		res => {
+        		console.log(res);
+        		if(!res.errors) {
+        			this.router.navigate(['login']);
+        		} else {
+          			this.errorMessage = res.errors[0].msg;
+        		}
+      		},
+      		error=>console.error(error)
+    	)
+  	}
 }
