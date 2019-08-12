fb876d753e158e640890924e741e2c7ddbd2ee47
Ricky Goacher <rickyleegoacher@hotmail.co.uk>
2019-08-12 15:44:28 +0100

    Added create project

--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Project } from '../models/project.model';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,30 @@ export class ProjectsService {
 
 	apiURL: string = 'http://localhost:3000/api'
 
-	constructor(private httpClient: HttpClient) { }
+	constructor(private httpClient: HttpClient, private router: Router) { }
 
 	getProjects():Observable<Project[]> {
 		return this.httpClient.get<Project[]>(`${this.apiURL}/projects`);
 	}
+
+    addProject(title, description, url, icon) {
+      let headers = new HttpHeaders({
+        'Content-Type': 'application/json'
+      });
+      let options = {
+        headers: headers
+       }
+      const project = {
+        title: title,
+        description: description,
+        url: url,
+        icon: icon
+      };
+      return this.httpClient.post(`${this.apiURL}/projects/create`, project, options)
+      .subscribe(data => {
+        console.log(data);
+        this.router.navigate(['/']);
+      });
+    }
 
 }
