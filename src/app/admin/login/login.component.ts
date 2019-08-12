259b2efb94b40fb9ff2d9a7ed1e3a7a71da976b2
Ricky Goacher <rickyleegoacher@hotmail.co.uk>
2019-08-12 13:54:47 +0100

    Login component subscribed to userservice

--- src/app/admin/login/login.component.ts
+++ src/app/admin/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UserService } from '../../services/user.service';
+import * as jwt_decode from "jwt-decode";
 
 @Component({
   selector: 'app-login',
@@ -13,9 +16,37 @@ export class LoginComponent implements OnInit {
     	password:new FormControl(null, Validators.required)
   	});
 
-  constructor() { }
+  constructor(private router:Router, private userService:UserService) { }
+
+	public errorMessage: string = '';
 
   ngOnInit() {
   }
+
+    moveToRegister(){
+    	this.router.navigate(['/register']);
+  	}
+
+  	onSubmit(){
+    	if(!this.loginForm.valid){
+     		console.log('Invalid');
+     	return;
+    	}
+
+    	this.userService.login(JSON.stringify(this.loginForm.value))
+    	.subscribe(
+    		res => {
+        		if(res.user) {
+        			sessionStorage.setItem('token', res.token)
+        			const decoded = jwt_decode(res.token)
+        			sessionStorage.setItem('expire', decoded.exp)
+       				this.router.navigate(['/'])
+        		} else {
+          			this.errorMessage = res.message;
+        		}
+      		},
+      		error=>console.error(error)
+    	)
+	}
 
 }
