976c71f008ee70f90b570ad497e841310d411002
Ricky Goacher <rickyleegoacher@hotmail.co.uk>
2019-08-12 17:24:43 +0100

    Added form group + create experiments service and subscribed

--- src/app/admin/create-experiment/create-experiment.component.ts
+++ src/app/admin/create-experiment/create-experiment.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { ExperimentsService } from '../../services/experiments.service';
+import { Experiment } from '../../models/experiment.model';
 
 @Component({
   selector: 'app-create-experiment',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateExperimentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private experimentService: ExperimentsService) {
+ 		this.editorForm = this.fb.group({
+      		title: ['', Validators.required ],
+      		description: ['', Validators.required ],
+      		content: '',
+      		url: ['', Validators.required ]
+    	}); 
+  }
+
+    editorForm: FormGroup;
+  	titleContent: string = null;
+  	urlContent: string = null;
+  	descriptionContent: string = null;
+  	iconContent: string = null;
 
   ngOnInit() {
+  	this.editorForm = new FormGroup({
+  		'description': new FormControl(null),
+  		'title': new FormControl(null),
+  		'url': new FormControl(null),
+  		'icon': new FormControl(null)
+  	})
   }
+
+  	onSubmit() {
+  		this.titleContent = this.editorForm.get('title').value,
+  		this.urlContent = this.editorForm.get('url').value,
+  		this.descriptionContent = this.editorForm.get('description').value,
+  		this.iconContent = this.editorForm.get('icon').value,
+  		this.experimentService.addExperiment(this.titleContent, this.descriptionContent, this.urlContent, this.iconContent)
+    }
 
 }
--- src/app/services/experiments.service.ts
+++ src/app/services/experiments.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Experiment } from '../models/experiment.model';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,30 @@ export class ExperimentsService {
 
   	apiURL: string = 'http://localhost:3000/api'
 
-	constructor(private httpClient: HttpClient) { }
+	constructor(private httpClient: HttpClient, private router: Router) { }
 
 	getExperiments():Observable<Experiment[]> {
 		return this.httpClient.get<Experiment[]>(`${this.apiURL}/experiments`);
 	}
+
+    addExperiment(title, description, url, icon) {
+      let headers = new HttpHeaders({
+        'Content-Type': 'application/json'
+      });
+      let options = {
+        headers: headers
+       }
+      const experiment = {
+        title: title,
+        description: description,
+        url: url,
+        icon: icon
+      };
+      return this.httpClient.post(`${this.apiURL}/experiments/create`, experiment, options)
+      .subscribe(data => {
+        console.log(data);
+        this.router.navigate(['/']);
+      });
+    }
 
 }
