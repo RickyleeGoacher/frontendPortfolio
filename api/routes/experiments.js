2e8c209385a5f12f7592941f6bc6c6f089519400
Ricky Goacher <rickyleegoacher@hotmail.co.uk>
2019-08-12 17:22:42 +0100

    Updated create experiment route + added jwt and auth

--- api/routes/experiments.js
+++ api/routes/experiments.js
@@ -2,6 +2,8 @@ const Experiments = require('../models/experiments-model');
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const jwt = require('jsonWebtoken');
+const { ensureAutenticated } = require('../config/auth');
 
 // Get Experiments
 
@@ -13,15 +15,21 @@ router.get('/', (req, res, next) => {
 
 // Post an experiment
 
-router.post('/create', (req, res, next) => {
-		let post = new Experiments(req.body);
-			post.save()
-				.then(post => {
-					res.status(200).json({'Experiment': 'Experiment added.'});
-				})
-				.catch(err => {
-					res.status(400).send('Unable to save to database');
-				});
+router.route('/create').post(ensureAutenticated, (req, res, next) => {
+	jwt.verify(req.token, process.env.SECRET, (err) => {
+        if(err) {
+          res.sendStatus(403);
+        } else {
+			let post = new Experiments(req.body);
+				post.save()
+					.then(post => {
+						res.status(200).json({'Experiment': 'Experiment added.'});
+					})
+					.catch(err => {
+						res.status(400).send('Unable to save to database');
+					});
+		}
+	});
 })
 
 module.exports = router;
