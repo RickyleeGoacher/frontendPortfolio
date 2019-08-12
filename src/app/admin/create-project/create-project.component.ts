43b3db05465b7ebcfbde14ced07d2fb71401d76c
Ricky Goacher <rickyleegoacher@hotmail.co.uk>
2019-08-12 15:42:39 +0100

    Form group and form controls added

--- src/app/admin/create-project/create-project.component.ts
+++ src/app/admin/create-project/create-project.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { ProjectsService } from '../../services/projects.service';
+import { Project } from '../../models/project.model';
 
 @Component({
   selector: 'app-create-project',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateProjectComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private projectService: ProjectsService) {
+ 		this.editorForm = this.fb.group({
+      		title: ['', Validators.required ],
+      		description: ['', Validators.required ],
+      		content: '',
+      		url: ['', Validators.required ]
+    	});  	
+  }
+
+    editorForm: FormGroup;
+  	titleContent: string = null;
+  	urlContent: string = null;
+  	descriptionContent: string = null;
+  	iconContent: string = null;
 
   ngOnInit() {
+  	this.editorForm = new FormGroup({
+  		'description': new FormControl(null),
+  		'title': new FormControl(null),
+  		'url': new FormControl(null),
+  		'icon': new FormControl(null)
+  	})
   }
+
+  	onSubmit() {
+  		this.titleContent = this.editorForm.get('title').value,
+  		this.urlContent = this.editorForm.get('url').value,
+  		this.descriptionContent = this.editorForm.get('description').value,
+  		this.iconContent = this.editorForm.get('icon').value,
+  		this.projectService.addProject(this.titleContent, this.descriptionContent, this.urlContent, this.iconContent)
+    }
 
 }
