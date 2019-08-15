94b471481e567ff350d701dcc8483072d9bf9679
Ricky Goacher <rickyleegoacher@hotmail.co.uk>
2019-08-15 03:47:45 +0100

    Created form submit + form builder to pull data from form

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { ContactService } from '../../services/contact.service';
 
 @Component({
   selector: 'app-contact-form',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private contactService: ContactService) { 
+        this.editorForm = this.fb.group({
+            name: ['', Validators.required ],
+            email: ['', Validators.required ],
+            message: ['', Validators.required ]
+        });
+  }
+
+    editorForm: FormGroup;
+    nameContent: string = null;
+    emailContent: string = null;
+    messageContent: string = null;
 
   ngOnInit() {
+    this.editorForm = new FormGroup({
+        'name': new FormControl(null),
+        'email': new FormControl(null),
+        'message': new FormControl(null)
+    });
   }
+
+    onSubmit() {
+        this.nameContent = this.editorForm.get('name').value,
+        this.emailContent = this.editorForm.get('email').value,
+        this.messageContent = this.editorForm.get('message').value,
+        this.contactService.sendMessage(this.nameContent, this.emailContent, this.messageContent)
+    }
 
 }
